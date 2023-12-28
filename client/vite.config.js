9a20981d7f2cc0da3aad81793fae615edeeb793f
guymorganb <codecrafters956@gmail.com>
2023-12-27 23:16:25 -0600

    runnig build

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    base: '/Norenwood/', //change your-repo-name to the name of your repository
+  }
 })
